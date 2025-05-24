f87ae7490c51b06116b4a8b23c1814350c3ef623
venkatasaimohith <159933461+venkatasaimohith@users.noreply.github.com>
2025-05-24 23:02:12 +0530

    Remove all files

--- generateHash.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const plainPassword = 'admin123';
-
-const hashedPassword = bcrypt.hashSync(plainPassword, 10);
-console.log('Hashed Password:', hashedPassword);
